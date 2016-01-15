4d86e94e84f49b12d45c457e54a25f3e4483f94b
Jeremy Bernard <jeremy.bernard@epitech.eu>
2016-01-15 20:38:00 +0100

    clean server.js

--- api/server.js
+++ api/server.js
@@ -1,10 +1,9 @@
-
 var express = require('express');
 
 var app = express();
 
 require('./routes')(app);
 
-app.listen(8080);
-
-console.log('Listening on port 8080');
+app.listen(8080, function (){
+  console.log('Listening on port 8080');
+});
