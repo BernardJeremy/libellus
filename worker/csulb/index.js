4b6fc5051a1b87ce7bf4e0780bedf0bfd8126cd9
Adrien morel <adrien.morel@me.com>
2016-01-18 21:43:15 -0800

    update worker so it can parse every subject. This will launch 57 firefox so use at your own risks. In case of panic: pkill selenium-server; pkill firefox

--- worker/csulb/index.js
+++ worker/csulb/index.js
@@ -1,3 +1,63 @@
+// in case of panic, pkill selenium-server + pkill firefox
+
+var subjects = [{name: 'Africana Studies', code: 'AFRS'},
+{name: 'Asian & Asian American Studies', code:'AAAS'},
+{name: 'Chicano & Latino Studies', code:'CHLS'},
+{name: 'Child Developmnt & Family Stds', code:'CDFS'},
+{name: 'Classics', code:'CLSC'},
+{name: 'Communication Studies', code:'COMM'},
+{name: 'Comparative World Literature', code:'CWL'},
+{name: 'Computer Engr & Computer Sci', code:'CECS'},
+{name: 'Concurrent Enrollment', code:'XYZ'},
+{name: 'Consumer Affairs', code:'CAFF'},
+{name: 'Counseling', code:'COUN'},
+{name: 'Criminal Justice', code:'CRJU'},
+{name: 'Dance', code:'DANC'},
+{name: 'Education - Early Childhood', code:'EDEC'},
+{name: 'Education - Elementary', code:'EDEL'},
+{name: 'Education - Reading', code:'EDRG'},
+{name: 'Education Specialist', code:'EDSP'},
+{name: 'Educational Psychology', code:'ED P'},
+{name: 'Electrical Engineering', code:'E E'},
+{name: 'English', code:'ENGL'},
+{name: 'Family & Consumer Sciences', code:'FCS'},
+{name: 'Finance', code:'FIN'},
+{name: 'French', code:'FREN'},
+{name: 'Geographic Information Science', code:'GISC'},
+{name: 'Geography', code:'GEOG'},
+{name: 'Geology', code:'GEOL'},
+{name: 'German', code:'GERM'},
+{name: 'Gerontology', code:'GERN'},
+{name: 'Graduate Studies', code:'G S'},
+{name: 'Health Care Administration', code:'HCA'},
+{name: 'Health Science', code:'H SC'},
+{name: 'History', code:'HIST'},
+{name: 'Hospitality Management', code:'HFHM'},
+{name: 'Information Systems', code:'I S'},
+{name: 'International Studies', code:'I/ST'},
+{name: 'Italian', code:'ITAL'},
+{name: 'Journalism', code:'JOUR'},
+{name: 'Kinesiology', code:'KIN'},
+{name: 'Liberal Studies', code:'L/ST'},
+{name: 'Management', code:'MGMT'},
+{name: 'Mathematics', code:'MATH'},
+{name: 'Mathematics Prebaccalaureate', code:'MAPB'},
+{name: 'Music', code:'MUS'},
+{name: 'Nutrition and Dietetics', code:'NUTR'},
+{name: 'Philosophy', code:'PHIL'},
+{name: 'Physics', code:'PHYS'},
+{name: 'Political Science', code:'POSC'},
+{name: 'Psychology', code:'PSY'},
+{name: 'Public Policy & Administration', code:'PPA'},
+{name: 'Recreation', code:'REC'},
+{name: 'Religious Studies', code:'R/ST'},
+{name: 'Sociology', code:'SOC'},
+{name: 'Spanish', code:'SPAN'},
+{name: 'Statistics', code:'STAT'},
+{name: 'Theatre Arts', code:'THEA'},
+{name: 'University', code:'UNIV'},
+{name: 'Womens Gender Sexuality Stds', code:'WGSS'}]
+
 var webdriverio = require('webdriverio');
 var options = {
   desiredCapabilities: {
@@ -12,6 +72,12 @@ var config = {
 }
 var model = require('./model.js')
 
+for (var i = 0; i < subjects.length; i++) {
+  parse_subject(subjects[i], 0);
+}
+
+function parse_subject(subject, tr) {
+
 var client = webdriverio.remote(options)
 client.init()
 .url('https://my.cms.csulb.edu')
@@ -25,12 +91,12 @@ client.init()
 .waitForExist('#CLASS_SRCH_WRK2_STRM$45$', 5000)
 .selectByValue('#CLASS_SRCH_WRK2_STRM$45$', '2162')
 .pause(1000)
-.selectByValue('#SSR_CLSRCH_WRK_SUBJECT_SRCH$0', 'CECS')
+.selectByValue('#SSR_CLSRCH_WRK_SUBJECT_SRCH$0', subject.code)
 .selectByValue('#SSR_CLSRCH_WRK_INSTRUCTION_MODE$3', 'P')
 .click('#SSR_CLSRCH_WRK_SSR_OPEN_ONLY$4')
 .click('#CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH')
-.waitForExist('##ICSave', 6000)
-.click('##ICSave')
+.waitForExist('##ICSave', 3000).catch(function(err) {})
+.click('##ICSave').catch(function(err) {})
 .waitForExist('#DERIVED_REGFRM1_SS_TRANSACT_TITLE', 30000)
 .execute(function() {
   var scriptElt = document.createElement('script');
@@ -56,8 +122,8 @@ client.init()
   })
 }).then(function(result) {
   return Promise.all([
-    model.saveClasses({name: 'Spring 2016'}, {code: 'CECS', name: 'Computer Engr & Computer Sci'}, result.value),
-    model.getSlotsLastUpdateDate({code: 'CECS', name: 'Computer Engr & Computer Sci'})
+    model.saveClasses({name: 'Spring 2016'}, subject, result.value),
+    model.getSlotsLastUpdateDate(subject)
   ]).then((results) => {
     var slotsUpdateDates = results[1]
 
@@ -87,7 +153,7 @@ client.init()
             capacity: Number($('#SSR_CLS_DTL_WRK_ENRL_CAP').text())
           };
         }).then(function(result) {
-          model.saveSlotInfo({code: 'CECS', name: 'Computer Engr & Computer Sci'}, result.value)
+          model.saveSlotInfo(subject, result.value)
         })
         .waitForExist('#CLASS_SRCH_WRK2_SSR_PB_BACK', 30000)
         .click('#CLASS_SRCH_WRK2_SSR_PB_BACK');
@@ -96,7 +162,10 @@ client.init()
   })
 
 }).catch(function(error) {
-  console.log('Got error:', error, error.stack);
+  console.log('Got error with ' + subject.name + ', trying again.', error, error.stack);
+  if (tr < 5)
+    parse_subject(subject, tr + 1);
 });
 
 // .end();
+}
